f3ba17d7c149965dcf15c4fec667c21b8f580086
dezred <dezred@8a4de4d8-bc41-11de-869a-473eb4776397>
2011-04-03 10:48:09 +0000

    Added a hotkey for opening Add Contact Dialog

--- trunk/AddContactPlus/addcontactplus.h
+++ trunk/AddContactPlus/addcontactplus.h
@@ -32,6 +32,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <m_clist.h>
 #include <m_database.h>
 #include <m_genmenu.h>
+#include <m_hotkeys.h>
 #include <m_icolib.h>
 #include <m_langpack.h>
 #include <m_protocols.h>
