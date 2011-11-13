7fbc20fbe5a1629693031e53ade99ab75489228e
dezred@gmail.com <dezred@gmail.com@8a4de4d8-bc41-11de-869a-473eb4776397>
2011-11-13 23:57:17 +0000

    Added support for custom langpacks

--- trunk/SimpleStatusMsg/commonheaders.h
+++ trunk/SimpleStatusMsg/commonheaders.h
@@ -17,8 +17,11 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 // to enable all 0.9.0 core functions
 #define MIRANDA_VER 0x0900
+// to enable custom langpacks
+#define MIRANDA_CUSTOM_LP
 
 #define _WIN32_IE 0x0501
 
